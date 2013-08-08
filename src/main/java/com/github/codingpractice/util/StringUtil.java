76c36b46e655d4015fc367294817b1cdbbd36020
Patrick Kranz <patrick@got-email.de>
2013-08-08 08:10:57 +0200

    implemented check for anagrams

---
+++ src/main/java/com/github/codingpractice/util/StringUtil.java
@@ -0,0 +1,54 @@
+package com.github.codingpractice.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Patrick Kranz
+ */
+public final class StringUtil {
+    private StringUtil() {
+        // prevent object creation
+    }
+
+    public static boolean isAnagram(String first, String second) {
+        if (first == null || second == null) {
+            return false;
+        }
+
+        Map<Character, Integer> characterCount = new HashMap<>();
+        for (int i = 0; i<first.length(); i++) {
+            Character current = first.charAt(i);
+            if ((current >= 'a' && current <= 'z') || (current <= 'A' && current <= 'Z')) {
+                incrementCharacter(characterCount, current);
+            }
+        }
+
+        for (int i=0; i<second.length(); i++) {
+            Character current = first.charAt(i);
+            if ((current >= 'a' && current <= 'z') || (current <= 'A' && current <= 'Z')) {
+                if (characterCount.containsKey(current)) {
+                    Integer newValue = characterCount.get(current) - 1;
+                    if (newValue == 0) {
+                        characterCount.remove(current);
+                    } else {
+                        characterCount.put(current, newValue);
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void incrementCharacter(Map<Character, Integer> characters,
+                                           Character toIncrement) {
+        if (characters.containsKey(toIncrement)) {
+            characters.put(toIncrement, characters.get(toIncrement) + 1);
+        } else {
+            characters.put(toIncrement, 1);
+        }
+    }
+}
---
+++ src/test/java/com/github/codingpractice/util/StringUtilTest.java
@@ -0,0 +1,35 @@
+package com.github.codingpractice.util;
+
+import org.junit.Test;
+
+import static com.github.codingpractice.util.StringUtil.isAnagram;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Patrick Kranz
+ */
+public class StringUtilTest {
+
+    @Test
+    public void shouldReturnFalseWhenNullAsParameterGiven() {
+        assertFalse(isAnagram(null, ""));
+        assertFalse(isAnagram(null, null));
+        assertFalse(isAnagram("", null));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTwoNonAnagramStringsGiven() {
+        assertFalse(isAnagram("abc", "def"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTwoAnagramStringsGiven() {
+        assertTrue(isAnagram("abc", "bca"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTwoAnagramStringsWithWhitespaceGiven() {
+        assertTrue(isAnagram("abc cde", "cdeabc"));
+    }
+}
